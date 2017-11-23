8b15eef49fbf704cb65c02511cc7e00687f4364e
Edson José dos Santos <edsonj82@outlook.com>
2017-11-22 23:27:30 -0200

    Config Action Cable

---
+++ app/jobs/message_broadcast_job.rb
@@ -0,0 +1,10 @@
+def perform(message)
+   m = message.messagable
+   chat_name = (m.class == Channel)? "#{m.team.id}_channels_#{m.id}" : "#{m.team.id}_talks_#{m.id}"
+   ActionCable.server.broadcast(chat_name, {
+                                         message: message.body,
+                                         date: message.created_at.strftime("%d/%m/%y"),
+                                         name: message.user.name
+                                       })
+ end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  mount ActionCable.server => '/cable'
   root to: 'teams#index'
-  resources :teams, only: [:create, :destroy]
   get '/:slug', to: 'teams#show'
+  resources :teams, only: [:create, :destroy]
   resources :channels, only: [:show, :create, :destroy]
   resources :talks, only: [:show]
   resources :team_users, only: [:create, :destroy]
